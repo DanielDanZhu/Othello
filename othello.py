3fbd643410e18055689f6dbb68f4e4788474ff3b
Daniel Zhu <dzdude77@gmail.com>
2018-09-27 14:01:17 -0400

    Fixed naming conventions

--- othello.py
+++ othello.py
@@ -16,14 +16,14 @@ class Board:
     def __init__(self):
         self.pieces = [Piece(4, 4, 'b'), Piece(5, 5, 'b'), Piece(4, 5, 'w'), Piece(5, 4, 'w')]
 
-    def whiteNum(self):
+    def white_num(self):
         count = 0
         for i in self.pieces:
             if i.color == 'w':
                 count += 1
         return count
 
-    def blackNum(self):
+    def black_num(self):
         count = 0
         for i in self.pieces:
             if i.color == 'b':
@@ -36,10 +36,10 @@ class Board:
         for y in range(8, 0, -1):
             print(" " + str(y) + " | ", end = '')
             for x in range(1, 9, 1):
-                if self.getPiece(x, y):
-                    if self.getPiece(x, y).color == 'b':
+                if self.get_piece(x, y):
+                    if self.get_piece(x, y).color == 'b':
                         print('b', end = '')
-                    elif self.getPiece(x, y).color == 'w':
+                    elif self.get_piece(x, y).color == 'w':
                         print('w', end = '')
                 else:
                     print (' ', end = '')
@@ -47,58 +47,58 @@ class Board:
             print("\n   +---+---+---+---+---+---+---+---+")
         print("     1   2   3   4   5   6   7   8  ")
 
-    def getPiece(self, x, y):
+    def get_piece(self, x, y):
         for i in self.pieces:
             if i.x == x and i.y == y:
                 return i
         return False
 
-    def getColor(self, x, y):
+    def get_color(self, x, y):
         for i in self.pieces:
             if i.x == x and i.y == y:
                 return i.color
         return False
 
-    def inRange(self, x, y):
+    def in_range(self, x, y):
         return x >= 1 and x <= 8 and y >= 1 and y <= 8
 
-    def checkMove(self, x, y, color): # returns list of legal flips
-        if self.getPiece(x, y) or not self.inRange(x, y):
+    def check_move(self, x, y, color): # returns list of legal flips
+        if self.get_piece(x, y) or not self.in_range(x, y):
             return []
 
-        tilesToFlip = []
+        tiles_to_flip = []
         if color == 'w':
-            oppColor = 'b'
+            opp_color = 'b'
         else:
-            oppColor = 'w'
+            opp_color = 'w'
 
         for xdir, ydir in [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, 1], [1, 1], [1, -1], [-1, -1]]:
             xcurr, ycurr, count = x, y, 0
             xcurr += xdir
             ycurr += ydir
-            while self.inRange(xcurr, ycurr) and self.getColor(xcurr, ycurr) == oppColor:
+            while self.in_range(xcurr, ycurr) and self.get_color(xcurr, ycurr) == opp_color:
                 xcurr += xdir
                 ycurr += ydir
                 count += 1
-            if (self.getColor(xcurr, ycurr) == color and count > 0):
+            if (self.get_color(xcurr, ycurr) == color and count > 0):
                 xcurr -= xdir
                 ycurr -= ydir
                 while xcurr != x or ycurr != y:
-                    tilesToFlip.append([xcurr, ycurr])
+                    tiles_to_flip.append([xcurr, ycurr])
                     xcurr -= xdir
                     ycurr -= ydir
 
-        return tilesToFlip
+        return tiles_to_flip
 
-    def makeMove(self, flips, x, y, color): # flips coordinates in flip list
+    def make_move(self, flips, x, y, color): # flips coordinates in flip list
         for i in flips:
-            self.getPiece(i[0], i[1]).flip()
+            self.get_piece(i[0], i[1]).flip()
         self.pieces.append(Piece(x, y, color))
 
-    def checkLegal(self, color): # checkMove for all coords of a color
+    def check_legal(self, color): # check_move for all coords of a color
         for x in range(1, 9, 1):
             for y in range(1, 9, 1):
-                if self.checkMove(x, y, color) != []:
+                if self.check_move(x, y, color) != []:
                     return True
         return False
 
@@ -109,7 +109,7 @@ class Board:
         x, y, numFlips, turn = 0, 0, 0, 1
         col = ["w","b"]
 
-        while b.checkLegal("w") or b.checkLegal("b"):
+        while b.check_legal("w") or b.check_legal("b"):
             flips = []
 
             if turn % 2 == 1 or choice == 1:
@@ -132,54 +132,54 @@ class Board:
 
                     x = int(input("X: "))
                     y = int(input("Y: "))
-                    flips = b.checkMove(x, y, col[turn % 2])
-                    print(b.checkLegal(col[turn % 2]))
+                    flips = b.check_move(x, y, col[turn % 2])
+                    print(b.check_legal(col[turn % 2]))
                     first = False
-                b.makeMove(flips, x, y, col[turn % 2])
+                b.make_move(flips, x, y, col[turn % 2])
             elif choice == 2:
-                x, y, numFlips = b.beginnerAI(turn)
+                x, y, numFlips = b.beginner_AI(turn)
             elif choice == 3:
-                x, y, numFlips = b.intermediateAI(turn)
+                x, y, numFlips = b.intermediate_AI(turn)
 
-            if b.checkLegal(col[(turn + 1) % 2]):
+            if b.check_legal(col[(turn + 1) % 2]):
                 turn += 1
 
         os.system('cls')
         b.display()
         print("Game over")
-        print("Black pieces: " + str(b.blackNum()))
-        print("White pieces: " + str(b.whiteNum()))
+        print("Black pieces: " + str(b.black_num()))
+        print("White pieces: " + str(b.white_num()))
 
-        if b.blackNum() > b.whiteNum():
+        if b.black_num() > b.white_num():
             print("Black wins")
-        elif b.whiteNum() > b.blackNum():
+        elif b.white_num() > b.black_num():
             print("White wins")
         else:
             print("Tie")
 
-    def beginnerAI(self, turn):
+    def beginner_AI(self, turn):
         col = ['w', 'b']
         while True:
             x = random.randint(1, 8)
             y = random.randint(1, 8)
-            flips = b.checkMove(x, y, col[turn % 2])
+            flips = b.check_move(x, y, col[turn % 2])
             if flips != []:
                 break
-        b.makeMove(flips, x, y, col[turn % 2])
+        b.make_move(flips, x, y, col[turn % 2])
         return [x, y, len(flips)]
 
-    def intermediateAI(self, turn): # + randint(0, 6) % 8 + 1
+    def intermediate_AI(self, turn): # + randint(0, 6) % 8 + 1
         col = ['w', 'b']
         flips = []
         range = [1, 2, 3, 4, 5, 6, 7, 8]
         random.shuffle(range)
         for x in range:
             for y in range:
-                if len(b.checkMove(x, y, col[turn % 2])) > len(flips):
-                    flips = b.checkMove(x, y, col[turn % 2])
+                if len(b.check_move(x, y, col[turn % 2])) > len(flips):
+                    flips = b.check_move(x, y, col[turn % 2])
                     bestx = x
                     besty = y
-        b.makeMove(flips, bestx, besty, col[turn % 2])
+        b.make_move(flips, bestx, besty, col[turn % 2])
         return [bestx, besty, len(flips)]
 
 
